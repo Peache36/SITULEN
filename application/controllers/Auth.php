4fe0e35074c028f6eab37960d11831a500826bd2
Peache36 <erickgusnaldi@gmail.com>
2022-04-08 12:39:32 +0800

    Edit Profile

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,16 +11,75 @@ class Auth extends CI_Controller
 
     public function index()
     {
-        $data['title'] = 'Login';
-        $this->load->view('template/auth-header');
-        $this->load->view('auth/login');
-        $this->load->view('template/auth-footer');
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
+
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login Page';
+            $this->load->view('template/auth-header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('template/auth-footer');
+        } else {
+            $this->_login();
+        }
     }
 
+    // LOGIN
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        // JIKA ADA USER
+        if ($user) {
+            // JIKA AKTIF
+            if ($user['is_active'] == 1) {
+                // CEK PASSWORD
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    if ($user['role_id'] == 1) {
+                        redirect('admin');
+                    } else {
+                        redirect('user');
+                    }
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Wrong password !</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Your account has not been actived ! </div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Email is not registered ! </div>');
+            redirect('auth');
+        }
+    }
+
+    // END LOGIN
+
+    // REGISTRASI
     public function registrasi()
     {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
+
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|trim|valid_email');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered'
+        ]);
 
         $this->form_validation->set_rules('password1', 'password', 'required|trim|min_length[3]|matches[password2]', [
             'matches' => 'Password dont match !',
@@ -38,13 +97,36 @@ class Auth extends CI_Controller
                 'name' => htmlspecialchars($this->input->post('name', true)),
                 'email' => htmlspecialchars($this->input->post('email', true)),
                 'image' => 'default.jpg',
-                'passsword' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
                 'is_active' => 1,
                 'date_created' => time()
             ];
 
-            $this->db->inserts('user', $data);
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Your account has been created ! Please Login </div>');
+
+            redirect('auth');
         }
+    }
+
+    // END REGISTRASI
+
+    // LOGOUT
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> You has been log out </div>');
+        redirect('auth');
+    }
+    // END LOGOUT
+
+    // BLOCKED
+    public function blocked()
+    {
+        $this->load->view('auth/blocked');
     }
 }
